1af87a8b1a60415e0084393c0173313abf32924d
CorgoShip <lamacka.zbynek@gmail.com>
2020-05-13 17:25:48 +0200

    updated point

--- src/main/java/classes/Point.java
+++ src/main/java/classes/Point.java
@@ -3,6 +3,7 @@ package classes;
 public class Point {
     private Coordinate coordinate;
     private Boolean zastavit;
+    private String streetId;
 
     public Coordinate getCoordinate() {
         return coordinate;
@@ -12,10 +13,21 @@ public class Point {
         return zastavit;
     }
 
+    public String getStreetId() {
+        return streetId;
+    }
+
     public void print()
     {
         this.coordinate.printAll();
         System.out.println(this.zastavit);
+        System.out.println(this.streetId);
         System.out.println();
+    }
+
+    public Point(Coordinate coordinate, Boolean zastavit, String streetId) {
+        this.coordinate = coordinate;
+        this.zastavit = zastavit;
+        this.streetId = streetId;
     }
 }
